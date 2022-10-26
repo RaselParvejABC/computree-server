7aa259057fd33caea0d8ea8b2002b9813081ef0b
Rasel Parvej <RaselParvejABC@gmail.com>
2022-10-26 11:29:59 +0600

    Only Remains Filling  Data in JSON Files

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from "express";
+import courses from "./data/courses.json" assert { type: "json" };
 
 const app = express();
 const port = 5000;
 
-//app.use(express.static("public"));
-
 app.get("/", (req, res) => {
   res.send("CompuTree Server is Running Happily!");
+});
+
+app.get("/courses", (req, res) => {
+  res.json(courses);
+});
+
+app.get("/course/:id", (req, res) => {
+  res.json(courses.find((course) => course["id"] === req.params.id));
 });
 
 app.listen(port, () => {
